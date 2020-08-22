aabd452a5862349d53881e0fde0e9a1b91c317ec
Eshan-Feroz <66930705+Eshan-Feroz@users.noreply.github.com>
2020-08-21 19:46:40 -0700

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,54 @@
+class Form {
+    constructor() {
+      // elements of the form
+      this.input = createInput("Name");
+      this.button = createButton('Play');
+      this.greeting = createElement('h2');
+      this.title = createElement('h2');
+      this.reset = createButton("Reset")
+    }
+    hide(){
+      // Hides following elements of form
+      this.greeting.hide();
+      this.input.hide();
+      this.button.hide();
+    }
+  
+    display(){
+      //display the title, input, and play button
+      this.title.html("Car Racing Game");
+      this.title.position(displayWidth/2 - 50, 0);
+      
+      this.input.position(displayWidth/2 - 40, displayHeight/2 - 240);
+      this.button.position(displayWidth/2 + 30, displayHeight/2);
+      //this.reset.position(displayWidth/2 + 600, displayHeight/2 -430)
+      this.reset.position(displayWidth -100, 20)
+      
+      // hide input and play button if play button is pressed
+      this.button.mousePressed(() => {
+        this.input.hide();
+        this.button.hide();
+        
+        // set player name to the name the user gave
+        player.name = this.input.value();
+        
+        // increment the player count by one as each player joins
+        playerCount+=1;
+        player.index = playerCount;
+        // update the player's info and the count of each player
+        player.update()
+        player.updateCount(playerCount);
+
+        // display the greeting message for each player
+        this.greeting.html("Hello " + player.name)
+        this.greeting.position(displayWidth/2 - 70, displayHeight/4)
+      });
+      
+
+      this.reset.mousePressed(() =>{
+        player.updateCount(0);
+        game.update(0);
+      });
+
+    }
+  }
